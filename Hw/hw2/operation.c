b26fa53acbc1a9ad47302850234ade64a0363188
YiChiChao <katelyn.c0818@gmail.com>
2021-10-02 13:36:16 +0800

    circular linkedlist

---
+++ Hw/hw2/operation.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+    int value;
+    struct node* prev;
+    struct node* next;
+} Node;
+
+void insert(Node **head, int val1, int val2){
+    Node *nownode = *head;
+    while(val2 --){
+        Node* new = malloc(sizeof(Node));
+        new->value = val1;
+        new->next = nownode->next;
+        new->prev = nownode;
+        nownode->next->prev = new;
+        nownode->next = new;
+        nownode = new;
+    }
+}
+
+void erase(Node **head, int val){
+    Node *nownode = (*head)->next;
+    while(--val){
+        Node* delete = nownode;
+        nownode = nownode->next;
+        free(delete);
+    }
+    (*head)->next = nownode->next;
+    nownode->next->prev = *head;
+    free(nownode);
+}
+
+void move(Node **head, int val){
+    Node* nownode = *head;
+    Node* checknode = *head;
+    int length = 1;
+    while(checknode->next != *head){
+        ++length;
+        checknode = checknode->next;
+    }
+    val %= length;
+    while(val--){
+        nownode = nownode->next;
+    }
+    *head = nownode;
+}
+
+void show(Node **head){
+    Node *nownode = *head;
+    while(nownode->next != *head){
+        printf("%d ", nownode->value);
+        nownode = nownode->next;
+    }
+    printf("%d\n", nownode->value);
+}
+
+int main(){
+    Node* a;
+    int x, n;
+    char buf[100];
+    int num1, num2;
+    scanf("%d", &x);
+    a = malloc(sizeof(Node));
+    a->value = x;
+    a->next = a;//point to itself
+    a->prev = a;
+    scanf("%d", &n);
+    while(n--) {
+        scanf("%s",buf);
+        if(!strcmp(buf,"insert")) {
+            scanf("%d %d",&num1, &num2);
+            insert(&a, num1, num2); 
+        }
+        else if(!strcmp(buf,"erase")) {
+            scanf("%d",&num1);
+            erase(&a, num1); // erase1 <dest>
+        }
+        else if(!strcmp(buf,"move")) {
+            scanf("%d",&num1);
+            move(&a, num1); // erase2 <color>
+        }
+        else if(!strcmp(buf,"show")) {
+            show(&a);
+        }
+        
+    }
+    return 0;
+}
---
+++ Hw/hw2/typing.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _NODE{
+    char character;
+    struct _NODE *ptr_to_next_node;
+    struct _NODE *ptr_to_prev_node;
+} Node;
+
+#define next ptr_to_next_node 
+#define prev ptr_to_prev_node
+
+
+
+void insert(Node **cur, char c){
+    Node *nownode = malloc(sizeof(Node));
+    //point to itself -->only one node
+    nownode->character = c;
+    nownode->prev= *cur;
+    nownode->next = (*cur)->next;
+    (*cur)->next->prev = nownode;
+    (*cur)->next = nownode;
+    /*if(nownode->next == *cur){
+        tail = nownode;
+    }
+    if(nownode->prev == *cur){
+        head = nownode;
+    }*/
+}
+void backspace(Node **cur){
+    if((*cur) == head) return;
+    Node *delete = (*cur);
+    (*cur)->prev->next = (*cur)->next;
+    (*cur)->next->prev = (*cur)->prev;
+    *cur = delete->prev;
+    free(delete);
+    
+}
+
+void deletion(Node **cur){
+    if((*cur)->next == tail) return;
+   Node *delete = (*cur)->next;
+   (*cur)->next = delete->next;
+   delete->next->prev = *cur;
+   free(delete);
+}
+void go_left(Node **cur, int t){
+    Node *now = *cur;
+    for(int i = 0; i < t; ++i){
+        now = now->prev;
+    }
+    *cur = now;
+}
+void go_right(Node **cur, int t){
+    Node *now = *cur;
+    for(int i = 0; i < t; ++i){
+        now = now->next;
+    }
+    *cur = now;
+}
+void go_home(Node **cur){
+    *cur = head;
+}
+void go_end(Node **cur){
+    *cur = tail->prev;
+}
+
+void show()
+{
+    if(head == NULL)
+        {
+        printf("\n");
+        return;
+    }
+    Node *now = head->next;
+    while(now != head)
+    {
+        printf("%c ", now->character);
+        now = now->next;
+    }
+    printf("\n");
+}
+
+int n;
+char buf[100];
+Node* cursor;
+char intype;
+int num;
+Node *head;
+Node *tail; 
+
+int main()
+{
+    scanf("%d",&n);
+    head = (Node*)malloc(sizeof(Node));
+    head->next = head->prev = head;
+    cursor = tail = head;
+    while(n--)
+    {
+        scanf("%s",buf);
+        if(!strcmp(buf,"insert"))
+        {
+            getchar();
+            scanf("%c",&intype);
+            insert(&cursor, intype); 
+        }
+        else if(!strcmp(buf,"deletion"))
+        {
+            deletion(&cursor); 
+        }
+        else if(!strcmp(buf,"backspace"))
+        {
+            backspace(&cursor); 
+        }
+        else if(!strcmp(buf,"go_left"))
+        {
+            scanf("%d", &num);
+            go_left(&cursor, num); 
+        }
+        else if(!strcmp(buf,"go_right"))
+        {
+            scanf("%d", &num);
+            go_right(&cursor, num); 
+        }
+        else if(!strcmp(buf,"go_home"))
+        {
+            go_home(&cursor); 
+        }
+        else if(!strcmp(buf,"go_end"))
+        {
+            go_end(&cursor); 
+        }
+        else if(!strcmp(buf,"show"))
+        {
+            show();
+        }
+    }
+    return 0;
+}
