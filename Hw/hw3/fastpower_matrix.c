166bce115f26264d43aef050861185c311ca1ea2
YiChiChao <katelyn.c0818@gmail.com>
2021-10-27 16:50:13 +0800

    fastpower matrix

--- Hw/hw3/fastpower_matrix.c
+++ Hw/hw3/fastpower_matrix.c
@@ -1,67 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define Mod 1000000007
-
-typedef struct Mat{
-    long long int a[2][2];
+#include <stdio.h>
+#include <stdlib.h>
+#define MOD 1000000007
+typedef struct matrix{
+    long long int matrix[2][2];
 }Mat;
-Mat matmul(Mat A,Mat B,int mod);
-Mat fpw(Mat A,long long int n);
 
-Mat matmul(Mat A,Mat B,int mod){
-    int i=0,j=0,k=0;
-    Mat res;
-    for(i=0;i<2;++i){for(j=0;j<2;++j){res.a[i][j]=0;}}
-    for(i=0;i<2;++i){
-        for(j=0;j<2;++j){
-            for(k=0;k<2;++k){
-                res.a[j][i]+=(A.a[k][i]*B.a[j][k])%mod;//matrix multiply:(0,0)*(0,0)+(1,0)*(0,1)+(2,0)*(0,2)=>(k,i)*(j,k)
-            }
-            res.a[j][i]=res.a[j][i]%Mod;
+Mat *matrix_multiply(Mat *A, Mat *B){
+    Mat *result = malloc(sizeof(Mat));
+    //init result matrix
+    for(int i = 0; i < 2; ++i){
+        for(int j = 0; j < 2; ++j){
+            result->matrix[i][j] = 0;
         }
     }
-    return res;
+
+    for(int i = 0; i < 2; ++i){
+        for (int j = 0; j < 2; ++j){
+            for(int k = 0; k < 2; ++k){
+                result->matrix[i][j] += (A->matrix[i][k] * B->matrix[k][j]) % MOD;
+            }
+            result->matrix[i][j] = result->matrix[i][j] % MOD;
+        }
+    }
+    return result;
 }
 
-Mat fpw(Mat A,long long int n){
-    int i=0,j=0;
-    Mat res;
-    if(n==1){
+Mat *fpw(Mat *A, long long int k){
+    static int count = 0;
+    count++;
+    Mat *result = malloc(sizeof(Mat));
+    if(k == 1){
         return A;
     }
-    res=fpw(A,n/2);
-    if(n%2==0){
-        return matmul(res,res,Mod);
+    result = fpw(A, k/2);
+    if(k % 2 == 1){
+        return matrix_multiply(matrix_multiply(result, result), A);
     }
-    if(n%2==1){
-        return matmul(A,matmul(res,res,Mod),Mod);
-    }
+    else{
+        //printf("count = %d\n", count);
+        return matrix_multiply(result, result);
+    } 
 }
 
 int main(){
-    Mat A,B;
-    int i=0,j=0;
-    int t;
-    long long int k;//10^18
-    for(i=0;i<2;++i){
-        for(j=0;j<2;++j){
-            A.a[i][j]=0;
-            B.a[i][j]=0;
+    Mat *A = malloc(sizeof(Mat));
+    Mat *B = malloc(sizeof(Mat));
+    Mat *result = malloc(sizeof(Mat));
+    //init matrices
+    for(int i = 0; i < 2; ++i){
+        for(int j = 0; j < 2; ++j){
+            A->matrix[i][j] = 0;
+            B->matrix[i][j] = 0;
         }
     }
-    A.a[0][0]=1;
-    A.a[0][1]=1;
-    A.a[1][0]=1;
-    B.a[0][0]=1;
-    B.a[0][1]=1;
 
-    while(scanf("%lld",&k)!=EOF){
-        if(k<=2){
-            printf("%lld\n",B.a[0][2-k]);//because k-2 would be negative
-        }
+    //set the initial value of A and B;
+    A->matrix[0][0] = 1;
+    A->matrix[0][1] = 1;
+    A->matrix[1][0] = 1;
+    B->matrix[0][0] = 1;
+    B->matrix[1][0] = 1;
+
+    //printf the matrix
+    /*printf("A\n");
+    printf("\t%lld\t%lld\n",A->matrix[0][0], A->matrix[0][1]);
+    printf("\t%lld\t%lld\n",A->matrix[1][0], A->matrix[1][1]);
+    printf("B\n");
+    printf("\t%lld\t%lld\n",B->matrix[0][0], B->matrix[0][1]);
+    printf("\t%lld\t%lld\n",B->matrix[1][0], B->matrix[1][1]);
+    */
+    long long int k;
+    while(scanf("%lld", &k) != EOF){
+        if(k <= 2)printf("1\n");
         else{
-            printf("%lld\n",matmul(fpw(A,k-2),B,Mod).a[0][0]);
+            result = fpw(A, k-2);
+            result = matrix_multiply(result, B);
+            printf("%lld\n", result->matrix[0][0]);
         }
+        //print out the final value
+        
     }
-    return 0;
+    
+    
 }
