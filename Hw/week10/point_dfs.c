dd3422b44f9bb3ab9a06b3a3316cd49ce098a1eb
YiChiChao <katelyn.c0818@gmail.com>
2021-11-29 21:29:55 +0800

    #2

--- Hw/week10/point_dfs.c
+++ Hw/week10/point_dfs.c
@@ -1,45 +1,43 @@
-#include <stdio.h>
-#include <math.h>
-float distance[1005][1005];
-int visited[1005] = {0};
-int dfs(int now, int n, int r);
+#include<stdio.h>
+#include<math.h>
+int visited[1001] = {0};
+float length[1001][1001];
 int ally = 0;
-int independent = 0;
+int one = 0;
+int n, r;
+
+int dfs(int start){
+    int count = 0;
+    visited[start] = 1;
+    for(int i = 0; i < n; ++i){
+        if(visited[i] == 1)continue;
+        if(length[start][i] <= r){
+            count++;
+            dfs(i);
+        }
+    }
+    if(count == 0)return 0;
+    else return 1;
+}
 int main(){
-    int n;
-    int r;
-    int x[1005], y[1005];
+    int x[1001]={0};
+    int y[1001]={0};
+    
     scanf("%d %d", &n, &r);
     for(int i = 0; i < n; ++i){
         scanf("%d %d", &x[i], &y[i]);
     }
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
-            distance[i][j] = sqrt(pow((x[i]-x[j]), 2) + pow((y[i]-y[j]), 2));
+            length[i][j] = sqrt(pow(x[i]-x[j], 2) + pow(y[i]-y[j], 2));
         }
     }
     for(int i = 0; i < n; ++i){
         if(visited[i] == 1)continue;
-        else{
-            if(dfs(i, n, r) == 1)ally++;
-            else independent++;
-        }
+        if(dfs(i) == 1)ally++;
+        else one++;
     }
-    printf("%d %d\n", independent, ally);
-    return 0;
-}
+    printf("%d %d\n", one, ally);
 
-int dfs(int cur, int n, int r){
-    int count = 0;
-    visited[cur] = 1;
-    for(int i = 0; i < n; ++i){
-        if(i == cur)continue;
-        if(visited[i] == 1)continue;
-        if(distance[i][cur] <= r){
-            dfs(i, n, r);
-            count++;
-        }
-    }
-    if(count == 0)return 0;
-    else return 1;
+    return 0;
 }
